7ae183c7815364ca00eac1d2586edd3f52ed059e
New Random User Name For The Project <pauzern@bullshitsoftware.online>
2022-07-02 23:38:22 +0000

    👍🏿 Add some request assertions

--- main_test.go
+++ main_test.go
@@ -9,6 +9,57 @@ import (
 	"time"
 )
 
+var ytServer *http.Server
+
+func init() {
+	handler := http.NewServeMux()
+	handler.HandleFunc("/api/workItems", func(w http.ResponseWriter, r *http.Request) {
+		if v, ok := r.Header["Authorization"]; !ok && v[0] != "Bearer your-token" {
+			http.Error(w, "Missing or invalid \"Authorization\" header", http.StatusBadRequest)
+
+			return
+		}
+
+		if r.Method != "GET" {
+			http.NotFound(w, r)
+
+			return
+		}
+
+		q := r.URL.Query()
+		if v, ok := q["fields"]; !ok || v[0] != "duration(minutes)" {
+			http.Error(w, "Missing or invalid \"fields\" query parameter", http.StatusBadRequest)
+
+			return
+		}
+
+		if v, ok := q["author"]; !ok || v[0] != "your-user-uuid" {
+			http.Error(w, "Missing or invalid \"author\" query parameter", http.StatusBadRequest)
+
+			return
+		}
+
+		if v, ok := q["start"]; !ok || v[0] != "1643673600000" {
+			http.Error(w, "Missing or invalid \"start\" query parameter", http.StatusBadRequest)
+
+			return
+		}
+
+		if v, ok := q["end"]; !ok || v[0] != "1646092799000" {
+			http.Error(w, "Missing or invalid \"end\" query parameter", http.StatusBadRequest)
+
+			return
+		}
+
+		w.Write([]byte("[{\"duration\":{\"minutes\":80}}]"))
+	})
+
+	ytServer = &http.Server{
+		Addr:    "127.0.0.1:2378",
+		Handler: handler,
+	}
+}
+
 func TestMain(m *testing.M) {
 	now = time.Date(2022, 2, 24, 13, 13, 13, 13, time.Now().Location())
 	home = path.Join(os.TempDir(), "youtimetrack")
@@ -22,15 +73,10 @@ func Example() {
 	os.Args = []string{"yourtimetrack", "init"}
 	main()
 
-	http.HandleFunc("/api/workItems", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("[{\"duration\":{\"minutes\":80}}]"))
-	})
-	server := &http.Server{Addr: "127.0.0.1:2378"}
-	go server.ListenAndServe()
-
+	go ytServer.ListenAndServe()
 	os.Args = []string{"yourtimetrack"}
 	main()
-	server.Shutdown(context.TODO())
+	ytServer.Shutdown(context.TODO())
 
 	// Output: Created /tmp/youtimetrack/config.json
 	// 1h 20m / 143h / 159h (worked / today / month)
