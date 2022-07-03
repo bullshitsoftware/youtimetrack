7836adbff8d89f3629d3a60c40afa88eaf084c57
New Random User Name For The Project <pauzern@bullshitsoftware.online>
2022-07-03 15:26:06 +0000

    🚩 Add start and end cli flags

--- command.go
+++ command.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"path"
 	"time"
@@ -11,15 +12,46 @@ func Init(app *App) {
 	fmt.Println("Created " + path.Join(home, config))
 }
 
-func Summary(app *App) {
+func Summary(app *App, args []string) {
 	start, end := app.Calendar.Period(now)
+	fs := flag.NewFlagSet("summary", flag.ExitOnError)
+	fs.Func("start", "start date (2006-01-02)", func(s string) error {
+		var err error
+		start, err = time.Parse("2006-01-02", s)
+		if err != nil {
+			fmt.Printf("%v\n", start)
+			return err
+		}
+		return nil
+	})
+	fs.Func("end", "end date (2006-01-02)", func(s string) error {
+		var err error
+		end, err = time.Parse("2006-01-02", s)
+		if err != nil {
+			return err
+		}
+		end = time.Date(end.Year(), end.Month(), end.Day(), 23, 59, 59, 0, time.UTC)
+		return nil
+	})
+	fs.Parse(args)
+
 	month := app.Calendar.Calc(start, end)
-	today := app.Calendar.Calc(start, now)
 	items := app.Youtrack.Fetch(start, end)
 	worked := 0
 	for _, i := range items {
 		worked += i.Duration.Minutes
 	}
+
+	if now.Before(start) || now.After(end) {
+		fmt.Printf("%s / %s (worked / month)\n",
+			FormatMinutes(worked),
+			FormatMinutes(month),
+		)
+
+		return
+	}
+
+	today := app.Calendar.Calc(start, now)
 	fmt.Printf("%s / %s / %s (worked / today / month)\n",
 		FormatMinutes(worked),
 		FormatMinutes(today),
@@ -27,8 +59,29 @@ func Summary(app *App) {
 	)
 }
 
-func Details(app *App) {
+func Details(app *App, args []string) {
 	start, end := app.Calendar.Period(now)
+	fs := flag.NewFlagSet("details", flag.ExitOnError)
+	fs.Func("start", "start date (2006-01-02)", func(s string) error {
+		var err error
+		start, err = time.Parse("2006-01-02", s)
+		if err != nil {
+			fmt.Printf("%v\n", start)
+			return err
+		}
+		return nil
+	})
+	fs.Func("end", "end date (2006-01-02)", func(s string) error {
+		var err error
+		end, err = time.Parse("2006-01-02", s)
+		if err != nil {
+			return err
+		}
+		end = time.Date(end.Year(), end.Month(), end.Day(), 23, 59, 59, 0, time.UTC)
+		return nil
+	})
+	fs.Parse(args)
+
 	items := app.Youtrack.Fetch(start, end)
 	for _, i := range items {
 		date := time.Unix(i.Date/1000, 0)
--- main.go
+++ main.go
@@ -2,23 +2,30 @@ package main
 
 import (
 	"os"
+	"strings"
 )
 
 func main() {
 	app := Default()
 
-	cmd := "summary"
-	if len(os.Args) > 1 {
+	var args []string
+	var cmd string
+	if len(os.Args) > 1 && !strings.HasPrefix(os.Args[1], "-") {
 		cmd = os.Args[1]
+		args = os.Args[2:]
+	} else {
+		cmd = "summary"
+		args = os.Args[1:]
 	}
+
 	switch cmd {
 	case "init":
 		Init(app)
 	case "details":
 		app.ReadConfig()
-		Details(app)
+		Details(app, args)
 	case "summary":
 		app.ReadConfig()
-		Summary(app)
+		Summary(app, args)
 	}
 }
